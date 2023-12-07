831a7c7c806f9d46b5db224e045ed3a3ddae54d1
umi2002 <jonathanhoang2002@gmail.com>
2023-12-07 11:14:41 -0500

    Refactoring

--- database/importdb.js
+++ database/importdb.js
@@ -1,10 +1,9 @@
-import fetch from 'node-fetch';
-import { MongoClient } from 'mongodb';
+import fetch from "node-fetch";
+import { MongoClient, ServerApiVersion } from "mongodb";
+await import("dotenv/config");
 
-const MONGO_URL = "mongodb://localhost:27017";
-const SERVER_URL = "http://localhost:3001";
-const ROOT_ROUTE = "/gw2";
-const ITEMS_ROUTE = "/items";
+const MONGO_URI = process.env.MONGO_URI;
+const GW2_API_URI = "https://api.guildwars2.com/v2/items";
 const DATABASE_NAME = "gw2";
 const COLLECTION_NAME = "items";
 
@@ -14,52 +13,71 @@ async function fetchWithExponentialBackoff(endpoint, initialDelay = 1) {
     while (true) {
         try {
             const response = await fetch(endpoint);
-            if (!response.ok) throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
-            return await response.json(); // directly returning the parsed JSON
+            if (!response.ok)
+                throw new Error(
+                    `HTTP Error: ${response.status} ${response.statusText}`,
+                );
+
+            return await response.json();
         } catch (error) {
-            console.error(`Fetch failed. Retrying in ${delay}ms...`);
-            await new Promise(res => setTimeout(res, delay));
+            console.error(`Fetch failed. Retrying in ${delay} ms...`);
+            await new Promise((res) => setTimeout(res, delay));
             delay *= 2;
         }
     }
 }
 
-async function importData(apiEndpoint) {
+async function importData(apiEndpoint, client) {
     let data;
     try {
+        console.log("Fetching data from:", apiEndpoint);
         data = await fetchWithExponentialBackoff(apiEndpoint);
     } catch (error) {
-        console.error('Failed to fetch data:', error);
+        console.error("Failed to fetch data:", error);
         return;
     }
 
-    const client = new MongoClient(MONGO_URL);
-
     try {
-        await client.connect();
-
-        const key = data["id"];
-        const name = data["name"];
+        const key = data.id;
+        const name = data.name;
 
         const collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME);
-        await collection.updateOne({ id: key }, { $set: { name: name } }, { upsert: true });
+        await collection.updateOne(
+            { id: key },
+            { $set: { name: name } },
+            { upsert: true },
+        );
     } catch (error) {
-        console.error('Failed to insert data into MongoDB:', error);
-    } finally {
-        await client.close();
+        console.error("Failed to insert data into MongoDB:", error);
     }
 }
 
 async function importDb() {
-    const itemsServerEndpoint = SERVER_URL + ROOT_ROUTE + ITEMS_ROUTE;
-    const itemsJson = await fetchWithExponentialBackoff(itemsServerEndpoint);
+    const client = new MongoClient(MONGO_URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
 
-    console.log('Importing data...');
+    try {
+        await client.connect();
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        return;
+    }
+
+    const itemsJson = await fetchWithExponentialBackoff(GW2_API_URI);
+
+    console.log("Importing data...");
 
     for (const key in itemsJson) {
-        await importData(itemsServerEndpoint + "/" + itemsJson[key]);
+        await importData(GW2_API_URI + "/" + itemsJson[key], client);
     }
-    console.log('Data imported successfully.');
+
+    await client.close();
+    console.log("Data imported successfully.");
 }
 
 importDb();
