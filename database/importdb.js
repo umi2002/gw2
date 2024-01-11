f70fdb47718a0b322a4546037269a64705be540e
umi2002 <jonathanhoang2002@gmail.com>
2024-01-11 11:06:09 -0500

    Script now imports data on recipes the item is used in

--- database/importdb.js
+++ database/importdb.js
@@ -6,113 +6,144 @@ const GW2_BASE_URI = "https://api.guildwars2.com/v2/";
 const GW2_ITEMS_ENDPOINT = "items/";
 const GW2_PRICES_ENDPOINT = "commerce/prices/";
 const GW2_RECIPES_ENDPOINT = "recipes/";
-const GW2_RECIPE_SEARCH_ENDPOINT = "recipes/search?output=";
+const GW2_RECIPE_SEARCH_ENDPOINT = "recipes/search";
+const GW2_RECIPE_OUTPUT_ENDPOINT = "?output=";
+const GW2_RECIPE_INPUT_ENDPOINT = "?input=";
 const DATABASE_NAME = "gw2";
 const COLLECTION_NAME = "items";
 
 async function fetchWithExponentialBackoff(endpoint, initialDelay = 1) {
-    let delay = initialDelay;
+  let delay = initialDelay;
 
-    while (true) {
-        const response = await fetch(endpoint);
-        if (response.ok) {
-            return await response.json();
-        } else {
-            if (response.status === 404) {
-                return null;
-            } else {
-                console.error(`Fetch failed. Retrying in ${delay} ms...`);
-                await new Promise((res) => setTimeout(res, delay));
-                delay *= 2;
-            }
-        }
+  while (true) {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      if (response.status === 404) {
+        return [];
+      } else {
+        console.error(`Fetch failed. Retrying in ${delay} ms...`);
+        await new Promise((res) => setTimeout(res, delay));
+        delay *= 2;
+      }
     }
+  }
 }
 
 async function importData(id, client) {
-    let itemData;
-    let priceData;
-    let recipeData;
-    try {
-        console.log("Fetching data from id:", id);
-        itemData = await fetchWithExponentialBackoff(
-            GW2_BASE_URI + GW2_ITEMS_ENDPOINT + id,
-        );
-        console.log("Fetched item data:", itemData);
-        priceData = await fetchWithExponentialBackoff(
-            GW2_BASE_URI + GW2_PRICES_ENDPOINT + id,
-        );
-        console.log("Fetched price data:", priceData);
-        const recipeIds = await fetchWithExponentialBackoff(
-            GW2_BASE_URI + GW2_RECIPE_SEARCH_ENDPOINT + id,
-        );
-        console.log("Fetched recipe id:", recipeIds);
-        recipeData = await recipeIds.reduce(async (accPromise, id) => {
-            const acc = await accPromise; // Await necessary because callback is async
-            const data = await fetchWithExponentialBackoff(
-                GW2_BASE_URI + GW2_RECIPES_ENDPOINT + id,
-            );
-            acc.push(data.ingredients);
-            console.log("Fetched recipe ingredients:", data.ingredients);
-            return acc;
-        }, []);
-    } catch (error) {
-        console.error("Failed to fetch data:", error);
-        throw error;
-    }
+  let itemData;
+  let priceData;
+  let outputRecipeData;
+  let inputRecipeData;
 
-    try {
-        const key = itemData.id;
-        const name = itemData.name;
-        const icon = itemData.icon;
-        const tradeable = priceData !== null;
-        const recipes = recipeData || [];
-        const collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME);
-        await collection.updateOne(
-            { id: key },
-            {
-                $set: {
-                    name: name,
-                    icon: icon,
-                    tradeable: tradeable,
-                    recipes: recipes,
-                },
-            },
-            { upsert: true },
-        );
-    } catch (error) {
-        console.error("Failed to insert itemData into MongoDB:", error);
-    }
+  try {
+    console.log("Fetching data for item with id:", id);
+
+    itemData = await fetchWithExponentialBackoff(
+      GW2_BASE_URI + GW2_ITEMS_ENDPOINT + id,
+    );
+    console.log("itemData:", itemData);
+
+    priceData = await fetchWithExponentialBackoff(
+      GW2_BASE_URI + GW2_PRICES_ENDPOINT + id,
+    );
+    console.log("priceData:", priceData);
+
+    const outputRecipeIds = await fetchWithExponentialBackoff(
+      GW2_BASE_URI +
+        GW2_RECIPE_SEARCH_ENDPOINT +
+        GW2_RECIPE_OUTPUT_ENDPOINT +
+        id,
+    );
+    console.log("outputRecipeIds:", outputRecipeIds);
+
+    outputRecipeData = await outputRecipeIds.reduce(async (accPromise, id) => {
+      const acc = await accPromise;
+      const data = await fetchWithExponentialBackoff(
+        GW2_BASE_URI + GW2_RECIPES_ENDPOINT + id,
+      );
+      acc.push(data.ingredients);
+      return acc;
+    }, []);
+    console.log("outputRecipeData:", outputRecipeData);
+
+    const inputRecipeIds = await fetchWithExponentialBackoff(
+      GW2_BASE_URI +
+        GW2_RECIPE_SEARCH_ENDPOINT +
+        GW2_RECIPE_INPUT_ENDPOINT +
+        id,
+    );
+    console.log("inputRecipeIds:", inputRecipeIds);
+
+    inputRecipeData = await inputRecipeIds.reduce(async (accPromise, id) => {
+      const acc = await accPromise;
+      const data = await fetchWithExponentialBackoff(
+        GW2_BASE_URI + GW2_RECIPES_ENDPOINT + id,
+      );
+      acc.push(data.ingredients);
+      return acc;
+    }, []);
+    console.log("inputRecipeData:", inputRecipeData);
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    throw error;
+  }
+
+  try {
+    const key = itemData.id;
+    const name = itemData.name;
+    const icon = itemData.icon;
+    const tradeable = priceData !== [];
+    const outputRecipes = outputRecipeData;
+    const inputRecipes = inputRecipeData;
+    const collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME);
+    await collection.updateOne(
+      { id: key },
+      {
+        $set: {
+          name: name,
+          icon: icon,
+          tradeable: tradeable,
+          recipes: outputRecipes,
+          usedIn: inputRecipes,
+        },
+      },
+      { upsert: true },
+    );
+  } catch (error) {
+    console.error("Failed to insert itemData into MongoDB:", error);
+  }
 }
 
 async function importDb() {
-    const client = new MongoClient(MONGO_URI, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        },
-    });
+  const client = new MongoClient(MONGO_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
-    try {
-        await client.connect();
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        return;
-    }
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    return;
+  }
 
-    const itemsJson = await fetchWithExponentialBackoff(
-        GW2_BASE_URI + GW2_ITEMS_ENDPOINT,
-    );
+  const itemsJson = await fetchWithExponentialBackoff(
+    GW2_BASE_URI + GW2_ITEMS_ENDPOINT,
+  );
 
-    console.log("Importing data...");
+  console.log("Importing data...");
 
-    for (const key in itemsJson) {
-        await importData(itemsJson[key], client);
-    }
+  for (const key in itemsJson) {
+    await importData(itemsJson[key], client);
+  }
 
-    await client.close();
-    console.log("Data imported successfully.");
+  await client.close();
+  console.log("Data imported successfully.");
 }
 
 importDb();
